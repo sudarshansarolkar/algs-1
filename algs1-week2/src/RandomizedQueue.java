c2f2e650bc1acca29f7e40c2f851ef9421a05a7b
Sudarshan <Sudarshan@192.168.1.110>
2014-10-04 08:36:16 -0700

    week2

---
+++ algs1-week2/src/RandomizedQueue.java
@@ -0,0 +1,57 @@
+import java.util.Iterator;
+
+/**
+ * 
+ * @author Sudarshan
+ *
+ * @param <Item>
+ *            Throw a NullPointerException if the client attempts to add a null
+ *            item; throw a java.util.NoSuchElementException if the client
+ *            attempts to sample or dequeue an item from an empty randomized
+ *            queue; throw an UnsupportedOperationException if the client calls
+ *            the remove() method in the iterator; throw a
+ *            java.util.NoSuchElementException if the client calls the next()
+ *            method in the iterator and there are no more items to return.
+ */
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    public RandomizedQueue() // construct an empty randomized queue
+    {
+
+    }
+
+    public boolean isEmpty() // is the queue empty?
+    {
+
+    }
+
+    public int size() // return the number of items on the queue
+    {
+
+    }
+
+    public void enqueue(Item item) // add the item
+    {
+
+    }
+
+    public Item dequeue() // delete and return a random item
+    {
+
+    }
+
+    public Item sample() // return (but do not delete) a random item
+    {
+
+    }
+
+    public Iterator<Item> iterator() // return an independent iterator over
+                                     // items in random order
+    {
+
+    }
+
+    public static void main(String[] args) // unit testing
+    {
+
+    }
+}
