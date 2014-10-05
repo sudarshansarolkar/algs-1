7135811fb25dfed8270733dfaa6d8bbd02e26929
Sudarshan <Sudarshan@192.168.1.110>
2014-10-04 21:02:37 -0700

    submission1

--- algs1-week2/src/Deque.java
+++ algs1-week2/src/Deque.java
@@ -1,62 +1,128 @@
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 /**
  * 
  * @author Sudarshan
  *
  * @param <Item>
- *            Throw a NullPointerException if the client attempts to add a null
- *            item; throw a java.util.NoSuchElementException if the client
- *            attempts to remove an item from an empty deque; throw an
- *            UnsupportedOperationException if the client calls the remove()
- *            method in the iterator; throw a java.util.NoSuchElementException
- *            if the client calls the next() method in the iterator and there
- *            are no more items to return.
+ * 
+ *            Your deque implementation must support each deque operation in
+ *            constant worst-case time and use space proportional to the number
+ *            of items currently in the deque. Additionally, iterator
+ *            implementation must support the operations next() and hasNext()
+ *            (plus construction) in constant worst-case time and use a constant
+ *            amount of extra space per iterator.
  */
 public class Deque<Item> implements Iterable<Item> {
+
+    private LinkedList<Item> data;
+
     public Deque() // construct an empty deque
     {
-
+        data = new LinkedList<Item>();
     }
 
     public boolean isEmpty() // is the deque empty?
     {
-
+        return data.isEmpty();
     }
 
     public int size() // return the number of items on the deque
     {
-
+        return data.size();
     }
 
+    /**
+     * Throw a NullPointerException if the client attempts to add a null item;
+     * 
+     * @param item
+     */
     public void addFirst(Item item) // insert the item at the front
     {
-
+        if (item == null) {
+            throw new NullPointerException(
+                    "addFirst using null element is not allowed");
+        }
+        data.addFirst(item);
     }
 
     public void addLast(Item item) // insert the item at the end
     {
-
+        if (item == null) {
+            throw new NullPointerException(
+                    "addFirst using null element is not allowed");
+        }
+        data.addLast(item);
     }
 
+    /**
+     * throw a java.util.NoSuchElementException if the client attempts to remove
+     * an item from an empty deque;
+     */
     public Item removeFirst() // delete and return the item at the front
     {
-
+        if (isEmpty()) {
+            throw new NoSuchElementException(
+                    "attempt to remove element from empty queue");
+        }
+        return data.removeFirst();
     }
 
     public Item removeLast() // delete and return the item at the end
     {
-
+        if (isEmpty()) {
+            throw new NoSuchElementException(
+                    "attempt to remove element from empty queue");
+        }
+        return data.removeLast();
     }
 
+    /**
+     * throw an UnsupportedOperationException if the client calls the remove()
+     * method in the iterator; throw a java.util.NoSuchElementException if the
+     * client calls the next() method in the iterator and there are no more
+     * items to return.
+     */
+    @SuppressWarnings("unchecked")
     public Iterator<Item> iterator() // return an iterator over items in order
                                      // from front to end
     {
-
+        return new ListIterator<Item>((Item[]) data.toArray());
     }
 
     public static void main(String[] args) // unit testing
     {
 
     }
+}
+
+class ListIterator<E> implements Iterator<E> {
+
+    private E data[];
+    private int cur;
+
+    ListIterator(E[] data) {
+        this.data = data;
+        cur = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return (cur < data.length);
+    }
+
+    @Override
+    public E next() {
+        if (!hasNext()) {
+            throw new java.util.NoSuchElementException();
+        }
+        return data[cur++];
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+
 }
--- algs1-week2/src/RandomizedQueue.java
+++ algs1-week2/src/RandomizedQueue.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * 
@@ -14,44 +15,117 @@ import java.util.Iterator;
  *            method in the iterator and there are no more items to return.
  */
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] data; // array of items
+    private int N; // number of elements on stack
+
+    @SuppressWarnings("unchecked")
     public RandomizedQueue() // construct an empty randomized queue
     {
-
+        data = (Item[]) new Object[2];
     }
 
     public boolean isEmpty() // is the queue empty?
     {
+        return N == 0;
 
     }
 
     public int size() // return the number of items on the queue
     {
+        return N;
 
+    }
+
+    // resize the underlying array holding the elements
+    private void resize(int capacity) {
+        assert capacity >= N;
+        @SuppressWarnings("unchecked")
+        Item[] temp = (Item[]) new Object[capacity];
+        for (int i = 0; i < N; i++) {
+            temp[i] = data[i];
+        }
+        data = temp;
     }
 
     public void enqueue(Item item) // add the item
     {
+        if (N == data.length)
+            resize(2 * data.length); // double size of array if necessary
+        if (N > 1) {
+            int randLoc = StdRandom.uniform(N - 1);
+            Item temp = data[randLoc];
+            data[randLoc] = item;
+            data[N++] = temp; // add item
+        } else {
+            data[N++] = item;
+        }
 
     }
 
     public Item dequeue() // delete and return a random item
     {
+        if (isEmpty())
+            throw new NoSuchElementException("underflow");
+        Item item = data[N - 1];
+        data[N - 1] = null; // to avoid loitering
+        N--;
+        // shrink size of array if necessary
+        if (N > 0 && N == data.length / 4)
+            resize(data.length / 2);
+        return item;
 
     }
 
     public Item sample() // return (but do not delete) a random item
     {
+        if (isEmpty())
+            throw new NoSuchElementException("underflow");
+        return data[N - 1];
 
     }
 
     public Iterator<Item> iterator() // return an independent iterator over
                                      // items in random order
     {
-
+        return new RandListIterator<Item>(data, N);
     }
 
     public static void main(String[] args) // unit testing
     {
 
     }
+}
+
+class RandListIterator<E> implements Iterator<E> {
+
+    E data[];
+    private int cur;
+
+    @SuppressWarnings("unchecked")
+    RandListIterator(E[] d, int size) {
+        this.data = (E[]) new Object[size];
+        int index = 0;
+        for (int i = size - 1; i >= 0; i--) {
+            data[index++] = d[i];
+        }
+        cur = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return (cur < data.length);
+    }
+
+    @Override
+    public E next() {
+        if (!hasNext()) {
+            throw new java.util.NoSuchElementException();
+        }
+        return data[cur++];
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+
 }
